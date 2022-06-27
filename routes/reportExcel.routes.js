a706bfe11ef05702c403aea0eea6219120238faa
mariollori <mariollori@upeu.edu.pe>
2022-06-27 06:11:14 -0500

    xd

--- routes/reportExcel.routes.js
+++ routes/reportExcel.routes.js
@@ -6,11 +6,11 @@ const {checkToken } = require('../validation/token.validation')
 
 const reports = require('../controllers/reportExcel.controller')
 
-router.get('/estudiantesConPacientes',reports.estudiantes_con_pacientes_asignados);
-router.get('/psicologosConPacientes',reports.psicologos_con_pacientes_asignados);
+router.get('/estudiantesConPacientes',checkToken,reports.estudiantes_con_pacientes_asignados);
+router.get('/psicologosConPacientes',checkToken,reports.psicologos_con_pacientes_asignados);
 // router.get('/personasSolicitantes',reports.registro_personas_enProceso);   
-router.get('/personasAtendidasEgresados',reports.registro_personasAtendidasEgresados);   
-router.get('/personasAtendidasEstudiantes',reports.registro_personasAtendidasEstudiantes);   
-router.get('/personasCanceladas',reports.registro_datos_personas_canceladas);   
-router.get('/registroAtenciones',reports.registro_atenciones);   
+router.get('/personasAtendidasEgresados',checkToken,reports.registro_personasAtendidasEgresados);   
+router.get('/personasAtendidasEstudiantes',checkToken,reports.registro_personasAtendidasEstudiantes);   
+router.get('/personasCanceladas',checkToken,reports.registro_datos_personas_canceladas);   
+router.get('/registroAtenciones',checkToken,reports.registro_atenciones);   
 module.exports=router;
